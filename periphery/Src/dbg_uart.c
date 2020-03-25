59c3645d7a01c10d8eafacd5faee22dd0a354e50
martsin <mykhailo.m@i3engineering.com>
2020-03-25 23:20:49 +0200

    fix send dbg uart

--- periphery/Src/dbg_uart.c
+++ periphery/Src/dbg_uart.c
@@ -7,37 +7,25 @@
 
  static volatile uint8_t uart_busy_flag = 0;
 
-
-// void dbg_uart_start_receiving() {
-//   while(HAL_UART_Receive_IT(&huart2, &byte_buff, 1) != HAL_OK);
-// }
-
-
 void dbg_uart_TxEndCallback() {
   uart_busy_flag = 0;
 }
-
-void dbg_uart_RxEndCallback() {
-  //TODO обробка прийнятого байту
-  //HAL_UART_Receive_IT(&huart6, &byte_buff, 1);
-}
-
 
 static void __vprint(const char *fmt, va_list argp)
 {
     char string[PRINT_BUFFER_LEN];
     if(0 < vsprintf(string,fmt,argp)) 
     {
-        while(uart_busy_flag);
-        uart_busy_flag = 1;
         while(HAL_UART_Transmit_IT(&huart2, (uint8_t*)string, strlen(string)) != HAL_OK);
     }
 }
 
 void dbg_print(const char *fmt, ...)
 {
+    while(uart_busy_flag);
     va_list argp;
     va_start(argp, fmt);
     __vprint(fmt, argp);
     va_end(argp);
+    uart_busy_flag = 1;
 }
